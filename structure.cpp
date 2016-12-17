8c436d2c2cabcb88f20316b9360151cc42563916
Francis Adrian A. Labrador <f_labrador30@yahoo.com>
2016-12-17 16:48:34 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-using namespace std
+using namespace std;
 
 struct Person{
-	int toes;
-	int knees; 
-	int shoulder;
-	int head;
+	int toes=0;
+	int knees=0; 
+	int shoulder=0;
+	int head=0;
 };
+int main(){
+		Person parts;
+		cin>>parts.toes;
+		cin>>parts.knees;
+		cin>>parts.shoulder;
+		cin>>parts.head;
+	
+		cout<<parts.toes<<endl;
+		cout<<parts.knees<<endl;
+		cout<<parts.shoulder<<endl;
+		cout<<parts.head<<endl;
+	return 0;
+}
