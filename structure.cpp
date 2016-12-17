c992cc4afcf2edfbda290335e9ee341edbe8ae17
Francis Adrian A. Labrador <f_labrador30@yahoo.com>
2016-12-17 17:06:50 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -7,16 +7,20 @@ struct Person{
 	int shoulder=0;
 	int head=0;
 };
+
 int main(){
-		Person parts;
-		cin>>parts.toes;
-		cin>>parts.knees;
-		cin>>parts.shoulder;
-		cin>>parts.head;
-	
-		cout<<parts.toes<<endl;
-		cout<<parts.knees<<endl;
-		cout<<parts.shoulder<<endl;
-		cout<<parts.head<<endl;
+	Person parts[5];
+	for (int i=0; i<5; i++){
+	cin>>parts[i].toes;
+	cin>>parts[i].knees;
+	cin>>parts[i].shoulder;
+	cin>>parts[i].head;
+	}
+	for (int i=0; i<5; i++){
+	cout<<"Toes: "<<parts[i].toes<<endl;
+	cout<<"Knees: "<<parts[i].knees<<endl;
+	cout<<"Shoulder: "<<parts[i].shoulder<<endl;
+	cout<<"Head: "<<parts[i].head<<endl;
+	}
 	return 0;
 }
